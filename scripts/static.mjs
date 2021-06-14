8bb3a56e764f94cbb5430aba643e813df8822c43
sosukesuzuki <aosukeke@gmail.com>
2021-06-14 15:26:28 +0900

    Serve css

--- scripts/static.mjs
+++ scripts/static.mjs
@@ -12,13 +12,20 @@ const ST_PORT = 1337;
 
 const srcDir = path.join(__dirname, "../src");
 const tempDir = tempy.directory();
+
 const tmpIndexHtmlFilePath = path.join(tempDir, "index.html");
 fs.copyFileSync(path.join(srcDir, "index.html"), tmpIndexHtmlFilePath);
+fs.copyFileSync(
+  path.join(srcDir, "styles.css"),
+  path.join(tempDir, "styles.css")
+);
+
 const data = fs.readFileSync(tmpIndexHtmlFilePath, "utf-8");
 fs.writeFileSync(
   tmpIndexHtmlFilePath,
   data.replace("./index.js", `${DUCK_SERVE_URL}/compile?id=index`)
 );
+
 http
   .createServer(st({ path: tempDir, url: "/", index: "index.html" }))
   .listen(ST_PORT);
